7127e0112e1d1fb25e3791176b315bff16cf5fef
xuwenlin <479703955@qq.com>
2020-09-08 22:34:31 +0800

    完成了无重复字符的最长子串的练习 P3LongestSubstringWithoutRepeatingCharacters

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ leetcode算法题练习
 
 1、[两数之和](https://github.com/xuwenlin1127/leetcode/blob/master/src/com/xuwenlin/leetcode/easy/P1TwoSum.java)  
 2、[两数相加](https://github.com/xuwenlin1127/leetcode/blob/master/src/com/xuwenlin/leetcode/medium/P2AddTwoNumbers.java)
+2、[无重复字符的最长子串](https://github.com/xuwenlin1127/leetcode/blob/master/src/com/xuwenlin/leetcode/medium/P3LongestSubstringWithoutRepeatingCharacters.java)
 
---
+++ src/com/xuwenlin/leetcode/medium/P3LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,20 @@
+package com.xuwenlin.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P3LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+}
